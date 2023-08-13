6369b20cbb830ae5b4a10a42e3fed20c4a91ea4f
sandy saputra <ss5679848@gmail.com>
2023-08-13 14:29:18 +0700

    tes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const taskItems = document.querySelectorAll(".task-item");
     const tagElements = document.querySelectorAll(".tag");
+    const searchInput = document.getElementById("searchInput");
   
     // Add event listener to task items
     taskItems.forEach((item) => {
@@ -15,7 +16,37 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       });
     });
+    const tasks = [
+      { name: "Task 1", description: "Description of Task 1" },
+      { name: "Task 2", description: "Description of Task 2" },
+      { name: "Task 3", description: "Description of Task 3" },
+      // Add more tasks as needed
+    ];
+    function renderTaskCards(tasks) {
+      taskContainer.innerHTML = "";
+      tasks.forEach(task => {
+        const taskCard = document.createElement("div");
+        taskCard.classList.add("task-card");
+        taskCard.innerHTML = `
+          <h3>${task.name}</h3>
+          <p>${task.description}</p>
+        `;
+        taskContainer.appendChild(taskCard);
+      });
+    }
   
+    // Initial rendering of task cards
+    renderTaskCards(tasks);
+  
+    // Search input event listener
+    searchInput.addEventListener("input", () => {
+      const searchTerm = searchInput.value.toLowerCase();
+      const filteredTasks = tasks.filter(task =>
+        task.name.toLowerCase().includes(searchTerm) ||
+        task.description.toLowerCase().includes(searchTerm)
+      );
+      renderTaskCards(filteredTasks);
+    });
     // Example: Toggle visibility of category tasks
     const categoryLabels = document.querySelectorAll(".category");
     categoryLabels.forEach((label) => {
